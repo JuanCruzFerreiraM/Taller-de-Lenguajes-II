594d8e7f2a75568c0b11525a62c2e524968def39
Juan Cruz Ferreira Monteiro <juancruzferreiramonteiro04@gmail.com>
2023-09-04 22:57:21 -0300

    Se agregan los codigos de la practica 2

---
+++ Practica 2/Codigo/src/BouncyBalls.java
@@ -0,0 +1,47 @@
+
+public class BouncyBalls {
+     
+	public static void main(String[] args) {
+		 // set the scale of the coordinate system
+        StdDraw.setXscale(-2.0, 2.0);
+        StdDraw.setYscale(-2.0, 2.0);
+
+        // initial values
+        double posicionX = 0.480, posicionY = 0.860;     // position
+        double veloX = 0.1, veloY = 0.1;     // velocity
+        double radius = 0.05;              // radius
+
+        // main animation loop
+        idk(posicionX, posicionY, veloX, veloY, radius); 		
+	}
+    
+	private static void idk(double posicionX, double posicionY, double veloX, double veloY, double radius) {
+		while (true)  { 
+
+            // bounce off wall according to law of elastic collision
+            if (Math.abs(posicionX + veloX) > 2.0 - radius) veloX = -veloX;
+            if (Math.abs(posicionY + veloY) > 2.0 - radius) veloY = -veloY;
+
+            // update position
+            posicionX = posicionX + veloX; 
+            posicionY = posicionY + veloY; 
+
+            dibujaCirculo(posicionX, posicionY, radius); 
+            dibujaCirculo(-posicionX, -posicionY, 2*radius);
+            
+        }
+	}
+
+	private static void dibujaCirculo(double posicionX, double posicionY, double radius) {
+		// clear the background
+		StdDraw.clear(StdDraw.GRAY);
+
+		// draw ball on the screen
+		StdDraw.setPenColor(StdDraw.RED); 
+		StdDraw.filledCircle(posicionX, posicionY, radius); 
+		
+		// display and pause for 20 ms
+		StdDraw.show(20);
+	}
+
+}
